71acdb9ee10e8dffbe194448479ecb7cef6e6abe
lingulist <mattis.list@lingpy.org>
2021-04-27 16:41:32 +0200

    update

--- src/cltoolkit/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from cltoolkit.models.sounds import Phoneme, Inventory
-from cltoolkit.models.language import * 
-from cltoolkit.models.form import Form
-from cltoolkit.models.concept import Concept
--- src/cltoolkit/models/sounds.py
+++
@@ -1,6 +0,0 @@
-"""
-Basic classes for handling sounds and sound inventories.
-"""
-
-from pyclts.inventories import Phoneme, Inventory
-
