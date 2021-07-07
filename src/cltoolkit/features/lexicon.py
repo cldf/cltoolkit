baa0df48dfb18e9094794a27a399145a04834086
Robert Forkel <xrotwang@googlemail.com>
2021-07-07 14:51:39 +0200

    work on feature doc

--- src/cltoolkit/features/lexicon.py
+++ src/cltoolkit/features/lexicon.py
@@ -4,6 +4,13 @@ from functools import partial
 
 
 def has_a_b_colexified(language, alist=None, blist=None, ablist=None):
+    """
+    :rtype: boolean
+    :return:
+    - True: the language colexifies the specified concepts
+    - False: the language does ot colexify the specified concepts
+    - None: the language does not have counterparts for the specified concepts
+    """
     aforms, bforms, abforms = [], [], []
     for xlist, xforms in zip([alist, blist, ablist], [aforms, bforms, abforms]):
         if xlist:
@@ -13,10 +20,9 @@ def has_a_b_colexified(language, alist=None, blist=None, ablist=None):
                         xforms += [form.form]
 
     if aforms and bforms:
-        return int(any(f in bforms for f in aforms))
+        return any(f in bforms for f in aforms)
     if abforms:
-        return 1
-    return "NA"
+        return True
 
 
 has_hand_arm_colexified = partial(
@@ -25,6 +31,8 @@ has_hand_arm_colexified = partial(
     blist=["HAND"],
     ablist=["ARM OR HAND"]
 )
+has_hand_arm_colexified.__doc__ = \
+    '    Does a single word denote both Hand and Arm?\n' + has_a_b_colexified.__doc__
 
 has_finger_hand_colexified = partial(
     has_a_b_colexified,
