3f70bea96f8de4efd0707dd018f0a2e47644c785
lingulist <mattis.list@lingulist.de>
2021-07-13 13:39:53 +0200

    update

--- src/cltoolkit/features/lexicon.py
+++ src/cltoolkit/features/lexicon.py
@@ -1,6 +1,7 @@
 """Miscellaneous features for lexical data.
 """
 from functools import partial
+from itertools import product
 
 
 def has_a_b_colexified(language, alist=None, blist=None, ablist=None):
@@ -24,30 +25,50 @@ def has_a_b_colexified(language, alist=None, blist=None, ablist=None):
     if abforms:
         return True
 
+def has_a_in_b(
+        language, alist=None, blist=None):
+    """
+    Check for partial colexification of a in b.
 
-has_hand_arm_colexified = partial(
+    :rtype: bool
+    """
+    aforms, bforms = [], []
+    for xlist, xforms in zip([alist, blist], [aforms, bforms]):
+        if xlist:
+            for x in xlist:
+                if x in language.concepts:
+                    for form in language.concepts[x].forms:
+                        xforms += [form.form]
+    for aform, bform in product(aforms, bforms):
+        if bform.startswith(aform) and len(aform) > 2 and len(bform) > 5:
+            return True
+    if aforms and bforms:
+        return False
+
+
+has_arm_and_hand_colexified = partial(
     has_a_b_colexified,
     alist=["ARM"],
     blist=["HAND"],
     ablist=["ARM OR HAND"]
 )
-has_hand_arm_colexified.__doc__ = \
+has_arm_and_hand_colexified.__doc__ = \
     '    Does a single word denote both Hand and Arm?\n' + has_a_b_colexified.__doc__
 
-has_finger_hand_colexified = partial(
+has_finger_and_hand_colexified = partial(
     has_a_b_colexified,
     alist=["FINGER", "FINGER OR TOE"],
     blist=["HAND", "HAND OR ARM"],
 )
 
-has_green_blue_colexified = partial(
+has_green_and_blue_colexified = partial(
     has_a_b_colexified,
     alist=["GREEN", "GREEN OR UNRIPE", "LIGHT GREEN"],
     blist=["BLUE", "LIGHT BLUE"],
     ablist=["BLUE OR GREEN"],
 )
 
-has_red_yellow_colexified = partial(
+has_red_and_yellow_colexified = partial(
     has_a_b_colexified,
     alist=["RED"],
     blist=["YELLOW", "BRIGHT YELLOW", "DARK YELLOW"],
@@ -58,4 +79,41 @@ has_leg_and_foot_colexified = partial(
     alist=["FOOT"],
     blist=["LEG"],
     ablist=["FOOT OR LEG"]
+)
+
+has_toe_and_foot_colexified = partial(
+        has_a_b_colexified,
+        alist=["FOOT", "FOOT OR LEG"],
+        blist=["TOE", "FINGER OR TOE"]
+)
+
+has_eye_in_tear = partial(
+        has_a_in_b,
+        alist=["EYE"],
+        blist=["TEAR (OF EYE)"]
+)
+
+has_water_in_tear = partial(
+        has_a_in_b,
+        alist=["WATER"],
+        blist=["TEAR (OF EYE)"]
+)
+
+has_bark_and_skin_colexified = partial(
+        has_a_b_colexified,
+        alist=["BARK", "BARK OR SHELL"],
+        blist=["SKIN", "SKIN (HUMAN)", "SKIN (ANIMAL)"],
+        clist=["BARK OR SKIN"]
+        )
+
+has_skin_in_bark = partial(
+        has_a_in_b,
+        alist=["SKIN", "SKIN (HUMAN)", "SKIN (ANIMAL)"],
+        blist=["BARK"]
+)
+
+has_tree_in_bark = partial(
+        has_a_in_b,
+        alist=["TREE", "TREE OR WOOD"],
+        blist=["BARK"]
 )
