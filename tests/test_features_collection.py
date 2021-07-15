24e6795a9bdc62d22295110d923488ab81b82443
lingulist <mattis.list@lingulist.de>
2021-07-15 09:48:26 +0200

    add tests for clts compat

--- tests/test_features_collection.py
+++ tests/test_features_collection.py
@@ -14,11 +14,21 @@ def test_concepticon(concepticon):
     valid_glosses = set([c.gloss for c in concepticon.conceptsets.values()])
     c = FeatureCollection.from_metadata(pkg_path / 'features' / 'features.json')
     for feature in c.features:
-        if feature.module.endswith("lexicon"):
+        if hasattr(feature.function, "keywords"):
             for key, value in feature.function.keywords.items():
-                if key in ["alist", "blist", "ablist"]:
+                if key in ["alist", "blist", "ablist", "concepts"]:
                     for concept in value:
                         if concept not in valid_glosses:
                             raise ValueError("gloss {0} not in concepticon".format(concept))
 
 
+def test_clts(clts):
+    c = FeatureCollection.from_metadata(pkg_path / 'features' / 'features.json')
+    for feature in c.features:
+        if hasattr(feature.function, "keywords"):
+            for key, value in feature.function.keywords.items():
+                if key in ["features"]:
+                    for featureset in value:
+                        for feature in featureset:
+                            if feature not in clts.bipa.feature_system:
+                                raise ValueError("feature value {0} not in clts".format(concept))
