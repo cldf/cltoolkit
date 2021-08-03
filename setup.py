b5bf8d7f9f88b62a4e04fa9be5b0c84906348b60
Robert Forkel <xrotwang@googlemail.com>
2021-08-03 17:07:04 +0200

    release 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cltoolkit',
-    version='0.1.0.dev0',
+    version='0.1.0',
     description='',
     author='',
     author_email='',
